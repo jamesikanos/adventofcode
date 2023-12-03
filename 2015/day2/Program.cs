fdf9568164152cb1665bd9dca010e76d256c4647
James Woodall <jamesikanos@gmail.com>
2023-12-03 15:26:37 +0100

    Day 2 of 2015

--- 2015/day2/Program.cs
+++ 2015/day2/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var lines = File.ReadAllLines("input.txt");
+
+var total = 0;
+var totalRibbon = 0;
+
+foreach (var line in lines)
+{
+    var dimensions = line.Split('x').Select(j => int.Parse(j)).ToList();
+
+    var l = dimensions[0];
+    var w = dimensions[1];
+    var h = dimensions[2];
+
+    // var total = (2 * l * w) + (2 * l * h) + (2 * h * l);
+
+    var side1 = 2 * l * w;
+    var side2 = 2 * w * h;
+    var side3 = 2 * h * l;
+
+    var slack = Math.Min(Math.Min(side1, side2), side3) / 2;
+
+    var subtotal = side1 + side2 + side3 + slack;
+
+    Console.WriteLine(subtotal);
+
+    total += subtotal;
+
+    // Ribbin and Bow calculation
+    var bow = l * w * h;
+
+    var allSides = new [] { l, w, h }.OrderBy(j => j).ToArray();
+
+    var calc = allSides[0] + allSides[0] + allSides[1] + allSides[1];
+
+    Console.WriteLine("Ribbon Length: " + (calc + bow));
+
+    totalRibbon += (calc + bow);
+
+}
+
+Console.WriteLine(total);
+Console.WriteLine(totalRibbon);
