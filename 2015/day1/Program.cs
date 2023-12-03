ca94a8fabf9374ca6fbeaa8cd212adf44f23f94a
James Woodall <jamesikanos@gmail.com>
2023-12-03 15:05:18 +0100

    Add 2015 Day 1

---
+++ 2015/day1/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+var lines = File.ReadAllLines("input.txt");
+
+var firstBasementIndex = 0;
+
+foreach (var line in lines)
+{
+    int index = 0;
+
+    // Read each char
+    int i = 0;
+    foreach (var character in line)
+    {
+        i++;
+        if (character == '(')
+        {
+            index++;
+        }
+        else if (character == ')')
+        {
+            index--;
+        }
+
+        if (index < 0 && firstBasementIndex == 0)
+        {
+            firstBasementIndex = i;
+        }
+    }
+
+    Console.WriteLine("Floor: " + index);
+    Console.WriteLine("First Basement Index: " + firstBasementIndex);
+}
+
