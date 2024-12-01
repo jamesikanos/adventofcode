51dcaf388a5a9088837dca6061d5ffeecf5d3344
James Woodall <jamesikanos@gmail.com>
2024-12-01 17:28:18 +0000

    Day1 2024

--- 2024/day1/Program.cs
+++ 2024/day1/Program.cs
@@ -1,2 +1,50 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Open the sample.txt in the same folder as the program
+// For each line, split by the space character
+// Remove duplicate entries
+
+using System.Diagnostics;
+
+var lines = File.ReadAllLines("input.txt");
+
+var leftList = new List<int>();
+var rightList = new List<int>();
+
+foreach (var line in lines)
+{
+    var parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    leftList.Add(int.Parse(parts[0].Trim()));
+    rightList.Add(int.Parse(parts[1].Trim()));
+}
+
+leftList.Sort();
+rightList.Sort();
+
+Debug.Assert(leftList.Count == rightList.Count);
+
+var distance = 0;
+
+for (var i = 0; i < leftList.Count; i++)
+{
+    var leftNumber = leftList[i];
+    var rightNumber = rightList[i];
+
+    var newDistance = Math.Abs(rightNumber - leftNumber);
+
+    distance += newDistance;
+}
+
+Console.WriteLine($"Part One - Distance - {distance}");
+
+var similarityScore = 0;
+
+for (var i = 0; i < leftList.Count; i++)
+{
+    var leftNumber = leftList[i];
+
+    // Find the number of times the left number appears in the right list
+    var rightCount = rightList.Count(x => x == leftNumber);
+
+    similarityScore += leftNumber * rightCount;
+}
+
+Console.WriteLine($"Part Two - Similarity Score - {similarityScore}");
